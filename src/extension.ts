afa6f420e3385146bd4ffcc43c2ec31716dca6a7
Steven Countryman <stevenco@microsoft.com>
2022-09-17 12:42:29 -0700

    Refactored for readability

--- src/extension.ts
+++ src/extension.ts
@@ -11,15 +11,101 @@ import {
 } from "vscode";
 import { GitExtension, Repository } from "./git";
 
-const disposables: Disposable[] = [];
-const tempDisposables: Disposable[] = [];
-let openTextDocPaths: string[] = [];
+const gitWatchers: Disposable[] = [];
+let fileOpenDisposable: Disposable;
+let fileCloseDisposable: Disposable;
 
-const PREV_BRANCH_NAME_KEY = "branchTabs_prevBranchName";
+const branchTabsKey = (branchName: string) => {
+  return `${branchName}_tabs`;
+};
 
-function getBranchDocsKey(branchName: string) {
-  return `branchTabs_${branchName}_openDocs`;
-}
+const getBranchMemoryPaths = (
+  context: ExtensionContext,
+  branchName: string
+) => {
+  return context.workspaceState.get<string[]>(branchTabsKey(branchName)) ?? [];
+};
+
+const storeBranchMemoryPaths = async (
+  context: ExtensionContext,
+  branchName: string,
+  branchMemoryPaths: string[]
+) => {
+  await context.workspaceState.update(
+    branchTabsKey(branchName),
+    branchMemoryPaths
+  );
+};
+
+const addFileToBranchMemory = async (
+  context: ExtensionContext,
+  branchName: string,
+  filePath: string
+) => {
+  const branchMemoryPaths = getBranchMemoryPaths(context, branchName);
+  if (!branchMemoryPaths.includes(filePath)) {
+    branchMemoryPaths.push(filePath);
+    await storeBranchMemoryPaths(context, branchName, branchMemoryPaths);
+  }
+};
+
+const removeFileFromBranchMemory = async (
+  context: ExtensionContext,
+  branchName: string,
+  filePath: string
+) => {
+  let branchMemoryPaths = getBranchMemoryPaths(context, branchName);
+  if (branchMemoryPaths.includes(filePath)) {
+    branchMemoryPaths = branchMemoryPaths.filter((path) => path !== filePath);
+    await storeBranchMemoryPaths(context, branchName, branchMemoryPaths);
+  }
+};
+
+const disposeFileWatchers = () => {
+  if (fileOpenDisposable) {
+    fileOpenDisposable.dispose();
+  }
+  if (fileCloseDisposable) {
+    fileCloseDisposable.dispose();
+  }
+};
+
+const resetBranchFileWatchers = (
+  context: ExtensionContext,
+  branchName: string
+) => {
+  disposeFileWatchers();
+
+  fileOpenDisposable = workspace.onDidOpenTextDocument((doc) => {
+    if (doc.uri.scheme === "file") {
+      addFileToBranchMemory(context, branchName, doc.uri.path);
+    }
+  });
+  fileCloseDisposable = workspace.onDidCloseTextDocument((doc) => {
+    if (doc.uri.scheme === "file") {
+      removeFileFromBranchMemory(context, branchName, doc.uri.path);
+    }
+  });
+};
+
+const restoreBranchMemTabs = async (
+  context: ExtensionContext,
+  branchName: string
+) => {
+  const currBranchKey = branchTabsKey(branchName);
+  const stashedDocPaths =
+    context.workspaceState.get<string[]>(currBranchKey) ?? [];
+  if (stashedDocPaths.length > 0) {
+    disposeFileWatchers();
+    await commands.executeCommand("workbench.action.closeAllEditors");
+
+    stashedDocPaths.forEach(async (path) => {
+      await workspace
+        .openTextDocument(Uri.file(path).with({ scheme: "file" }))
+        .then((doc) => window.showTextDocument(doc, { preview: false }));
+    });
+  }
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -33,80 +119,26 @@ export function activate(context: ExtensionContext) {
       (repository: Repository) => {
         repository.state.onDidChange(
           async () => {
-            const prevBranchName =
-              context.workspaceState.get<string>(PREV_BRANCH_NAME_KEY);
             const currBranchName = repository.state.HEAD?.name;
 
-            if (
-              prevBranchName &&
-              currBranchName &&
-              prevBranchName !== currBranchName
-            ) {
-              if (openTextDocPaths.length > 0) {
-                const prevBranchKey = getBranchDocsKey(prevBranchName);
-                await context.workspaceState.update(
-                  prevBranchKey,
-                  openTextDocPaths
-                );
-              }
+            if (currBranchName) {
+              await restoreBranchMemTabs(context, currBranchName);
 
-              openTextDocPaths = [];
-              tempDisposables.forEach((d) => d.dispose());
-              workspace.onDidOpenTextDocument(
-                (doc) => {
-                  if (doc.uri.scheme === "file") {
-                    openTextDocPaths.push(doc.uri.path);
-                  }
-                },
-                null,
-                tempDisposables
-              );
-              workspace.onDidCloseTextDocument(
-                (doc) => {
-                  if (doc.uri.scheme === "file") {
-                    openTextDocPaths = openTextDocPaths.filter(
-                      (s) => s !== doc.uri.path
-                    );
-                  }
-                },
-                null,
-                tempDisposables
-              );
-              const currBranchKey = getBranchDocsKey(currBranchName);
-              const stashedDocPaths =
-                context.workspaceState.get<string[]>(currBranchKey) ?? [];
-              if (stashedDocPaths.length > 0) {
-                await commands.executeCommand(
-                  "workbench.action.closeAllEditors"
-                );
-
-                stashedDocPaths.forEach(async (path) => {
-                  await workspace
-                    .openTextDocument(Uri.file(path).with({ scheme: "file" }))
-                    .then((doc) =>
-                      window.showTextDocument(doc, { preview: false })
-                    );
-                });
-              }
+              resetBranchFileWatchers(context, currBranchName);
             }
-
-            await context.workspaceState.update(
-              PREV_BRANCH_NAME_KEY,
-              currBranchName
-            );
           },
           null,
-          disposables
+          gitWatchers
         );
       },
       null,
-      disposables
+      gitWatchers
     );
   }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-  disposables.forEach((d) => d.dispose());
-  tempDisposables.forEach((d) => d.dispose());
+  disposeFileWatchers();
+  gitWatchers.forEach((d) => d.dispose());
 }
