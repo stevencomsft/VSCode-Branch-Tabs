ccdab9466deca837506a5121b133950cdcfe30a7
Steven Countryman <stevenco@microsoft.com>
2022-09-16 17:46:02 -0700

    Restoring sync file open to fix issue with files being out of sync

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import {
+  commands,
+  ExtensionContext,
+  extensions,
+  Uri,
+  workspace,
+} from "vscode";
 import { GitExtension, Repository } from "./git";
 
 const PREV_BRANCH_NAME_KEY = "branchTabs_prevBranchName";
@@ -9,27 +15,36 @@ function getBranchDocsKey(branchName: string) {
   return `branchTabs_${branchName}_openDocs`;
 }
 
+function openFilesInSync(uris: Uri[], index: number) {
+  if (index >= uris.length) {
+    return;
+  }
+
+  commands
+    .executeCommand("vscode.open", Uri.file(uris[index].path))
+    .then(() => {
+      commands.executeCommand("workbench.action.keepEditor").then(() => {
+        openFilesInSync(uris, index + 1);
+      });
+    });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   const gitExtension =
-    vscode.extensions.getExtension<GitExtension>("vscode.git")?.exports;
+    extensions.getExtension<GitExtension>("vscode.git")?.exports;
   const git = gitExtension?.getAPI(1);
 
   if (git) {
     git.onDidOpenRepository((repository: Repository) => {
-      context.workspaceState.update(
-        PREV_BRANCH_NAME_KEY,
-        repository.state.HEAD?.name
-      );
-
       repository.state.onDidChange(() => {
         const prevBranchName =
           context.workspaceState.get<string>(PREV_BRANCH_NAME_KEY);
         const currBranchName = repository.state.HEAD?.name;
 
         if (prevBranchName && prevBranchName !== currBranchName) {
-          const openDocUris = vscode.workspace.textDocuments
+          const openDocUris = workspace.textDocuments
             .map((doc) => doc.uri)
             .filter((uri) => uri.scheme === "file");
           if (openDocUris.length > 0) {
@@ -41,18 +56,12 @@ export function activate(context: vscode.ExtensionContext) {
         if (currBranchName && currBranchName !== prevBranchName) {
           const branchTabsKey = getBranchDocsKey(currBranchName);
           const prevBranchDocUris =
-            context.workspaceState.get<vscode.Uri[]>(branchTabsKey);
+            context.workspaceState.get<Uri[]>(branchTabsKey);
           if (prevBranchDocUris && prevBranchDocUris.length > 0) {
-            vscode.commands
+            commands
               .executeCommand("workbench.action.closeAllEditors")
               .then(() => {
-                prevBranchDocUris.forEach((docUri) => {
-                  vscode.workspace
-                    .openTextDocument(vscode.Uri.file(docUri.path))
-                    .then((doc) =>
-                      vscode.window.showTextDocument(doc, { preview: false })
-                    );
-                });
+                openFilesInSync(prevBranchDocUris, 0);
               });
           }
         }
