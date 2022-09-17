ab3d5d29514cec3fb3bf5527b708f07d8b47885d
Steven Countryman <stevenco@microsoft.com>
2022-09-17 07:34:03 -0700

    Now tracking files explicitely to get around VS Code file tracking issues

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,8 @@ import {
 import { GitExtension, Repository } from "./git";
 
 const disposables: Disposable[] = [];
+const tempDisposables: Disposable[] = [];
+let openTextDocPaths: string[] = [];
 
 const PREV_BRANCH_NAME_KEY = "branchTabs_prevBranchName";
 
@@ -27,48 +29,84 @@ export function activate(context: ExtensionContext) {
   const git = gitExtension?.getAPI(1);
 
   if (git) {
-    git.onDidOpenRepository((repository: Repository) => {
-      repository.state.onDidChange(() => {
-        const prevBranchName =
-          context.workspaceState.get<string>(PREV_BRANCH_NAME_KEY);
-        const currBranchName = repository.state.HEAD?.name;
+    git.onDidOpenRepository(
+      (repository: Repository) => {
+        repository.state.onDidChange(
+          async () => {
+            const prevBranchName =
+              context.workspaceState.get<string>(PREV_BRANCH_NAME_KEY);
+            const currBranchName = repository.state.HEAD?.name;
 
-        if (prevBranchName && prevBranchName !== currBranchName) {
-          const openDocUris = workspace.textDocuments
-            .map((doc) => doc.uri)
-            .filter((uri) => uri.scheme === "file");
-          if (openDocUris.length > 0) {
-            const branchTabsKey = getBranchDocsKey(prevBranchName);
-            context.workspaceState.update(branchTabsKey, openDocUris);
-          }
-        }
+            if (
+              prevBranchName &&
+              currBranchName &&
+              prevBranchName !== currBranchName
+            ) {
+              if (openTextDocPaths.length > 0) {
+                const prevBranchKey = getBranchDocsKey(prevBranchName);
+                await context.workspaceState.update(
+                  prevBranchKey,
+                  openTextDocPaths
+                );
+              }
 
-        if (currBranchName && currBranchName !== prevBranchName) {
-          const branchTabsKey = getBranchDocsKey(currBranchName);
-          const prevBranchDocUris =
-            context.workspaceState.get<Uri[]>(branchTabsKey);
-          if (prevBranchDocUris && prevBranchDocUris.length > 0) {
-            commands
-              .executeCommand("workbench.action.closeAllEditors")
-              .then(() => {
-                prevBranchDocUris.forEach(async (doc) => {
+              openTextDocPaths = [];
+              tempDisposables.forEach((d) => d.dispose());
+              workspace.onDidOpenTextDocument(
+                (doc) => {
+                  if (doc.uri.scheme === "file") {
+                    openTextDocPaths.push(doc.uri.path);
+                  }
+                },
+                null,
+                tempDisposables
+              );
+              workspace.onDidCloseTextDocument(
+                (doc) => {
+                  if (doc.uri.scheme === "file") {
+                    openTextDocPaths = openTextDocPaths.filter(
+                      (s) => s !== doc.uri.path
+                    );
+                  }
+                },
+                null,
+                tempDisposables
+              );
+              const currBranchKey = getBranchDocsKey(currBranchName);
+              const stashedDocPaths =
+                context.workspaceState.get<string[]>(currBranchKey) ?? [];
+              if (stashedDocPaths.length > 0) {
+                await commands.executeCommand(
+                  "workbench.action.closeAllEditors"
+                );
+
+                stashedDocPaths.forEach(async (path) => {
                   await workspace
-                    .openTextDocument(Uri.file(doc.path))
+                    .openTextDocument(Uri.file(path).with({ scheme: "file" }))
                     .then((doc) =>
                       window.showTextDocument(doc, { preview: false })
                     );
                 });
-              });
-          }
-        }
+              }
+            }
 
-        context.workspaceState.update(PREV_BRANCH_NAME_KEY, currBranchName);
-      }, disposables);
-    }, disposables);
+            await context.workspaceState.update(
+              PREV_BRANCH_NAME_KEY,
+              currBranchName
+            );
+          },
+          null,
+          disposables
+        );
+      },
+      null,
+      disposables
+    );
   }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
   disposables.forEach((d) => d.dispose());
+  tempDisposables.forEach((d) => d.dispose());
 }
